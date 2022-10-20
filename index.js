d5479fa25753c99c3cf25a076b67082083c407af
Emon02 <abdulalimemon11@gmail.com>
2022-10-21 03:09:18 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,6 +13,19 @@ app.use(express.json());
 // APIs
 app.get('/', (req, res) => {
     res.send('Running Ware House Server')
+});
+
+// MongoDb
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l8c46yf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(collection);
+  // perform actions on the collection object
+  client.close();
 });
 
 
